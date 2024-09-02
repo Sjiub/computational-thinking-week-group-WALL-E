2dae1caf3647450f61c7502c009d61077bec5b0b
Lyu-04 <leo.yu2@student.uva.nl>
2024-09-02 16:34:45 +0200

    Update git_exercise.py
    missing markus and melody

---
+++ challenge_day1/git_exercise.py
@@ -0,0 +1,13 @@
+from Khushi.py import my_name()
+from buijs.py import write_name()
+from leo.py import myname_leo()
+from shriya.py import my_name2()
+
+def team_names:
+  print("This is Team WALL-E, we are:")
+  my_name()
+  write_name()
+  myname_leo()
+  my_name2()
+
+team_names()
