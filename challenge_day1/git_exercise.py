10bac5f4bfd4be4a85c04d794a794daa0a681a6a
Lyu-04 <leo.yu2@student.uva.nl>
2024-09-02 17:05:13 +0200

    Update git_exercise.py

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -4,10 +4,9 @@ from leo import myname_leo
 from shriya import my_name2
 
 def team_names():
-  print("This is Team WALL-E, we are:")
-  my_name()
-  write_name()
-  myname_leo()
-  my_name2()
-
+    print("This is Team WALL-E, we are:")
+    print(my_name())       
+    print(write_name())    
+    print(myname_leo())    
+    print(my_name2())      
 team_names()
