1a292500ffd3c86701606114d0170b7f5ca309f2
schmarksuu <161357245+schmarksuu@users.noreply.github.com>
2024-09-02 17:29:07 +0200

    Update git_exercise.py

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -3,6 +3,7 @@ from buijs import write_name
 from leo import myname_leo
 from shriya import my_name2
 from melody import myname_3
+from markus import myname_4
 
 def team_names():
     print("This is Team WALL-E, we are:")
@@ -11,4 +12,6 @@ def team_names():
     print(myname_leo())    
     print(my_name2())
     print(myname_3())
+    print(myname_4())
+    
 team_names()
