051217e54dc4246b453b17ccb241535418915871
Lyu-04 <leo.yu2@student.uva.nl>
2024-09-02 16:48:45 +0200

    Update git_exercise.py
    corrected syntax errors oops

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,7 +1,7 @@
-from Khushi.py import my_name()
-from buijs.py import write_name()
-from leo.py import myname_leo()
-from shriya.py import my_name2()
+from Khushi import my_name
+from buijs import write_name
+from leo import myname_leo
+from shriya import my_name2
 
 def team_names:
   print("This is Team WALL-E, we are:")
