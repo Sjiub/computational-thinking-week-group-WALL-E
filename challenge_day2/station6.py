b71c055a9a593b440720dddc92434a01e63f2e47
Lyu-04 <leo.yu2@student.uva.nl>
2024-09-03 14:52:02 +0200

    Update station6.py

--- challenge_day2/station6.py
+++ challenge_day2/station6.py
@@ -2,7 +2,7 @@
 
 import math
 
-def solution_station_7(input_list):
+def solution_station_6(input_list):
     output_values = [round(math.sin(x), 4) for x in input_list]
     return output_values
 
