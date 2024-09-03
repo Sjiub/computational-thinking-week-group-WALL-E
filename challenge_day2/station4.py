d5a04dfa61be406b6ab6be129404a0426a5d83b8
Khushi <144587562+Khushiii891@users.noreply.github.com>
2024-09-03 16:26:33 +0700

    Update station4.py

--- challenge_day2/station4.py
+++ challenge_day2/station4.py
@@ -9,3 +9,15 @@ input          output
 0              F
 7              T
 """
+
+
+def solution_station_4(number):
+    if number <= 1:
+        print('F')
+    else:
+        for n in range(2, number):
+            if number % n == 0:
+                print('F')
+                break
+        else:
+            print('T')
