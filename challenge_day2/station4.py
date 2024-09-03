0573976e04d2d56e43ec046d7cbb62618fb74019
Lyu-04 <leo.yu2@student.uva.nl>
2024-09-03 09:51:37 +0200

    Update station4.py

---
+++ challenge_day2/station4.py
@@ -0,0 +1,11 @@
+# Boolean prime number stuff
+""" 
+input          output
+87             F
+37             T
+98             F
+26             F
+78             F
+0              F
+7              T
+"""
