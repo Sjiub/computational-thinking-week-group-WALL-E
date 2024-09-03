ff50a61e53d1e2e2abc1416bc8161bb70199f121
Lyu-04 <leo.yu2@student.uva.nl>
2024-09-03 09:42:09 +0200

    Update station2.py

---
+++ challenge_day2/station2.py
@@ -0,0 +1,13 @@
+# challenge japanese weekdays 
+# determine the weekday with the provided date format yyyy-mm-dd and translate to japanese
+# example data 
+# input 2023-01-10 output 火曜日 (Friday)
+""" 
+    Sunday＝日曜日（にちようび、nichiyoubi）
+    Monday＝月曜日（げつようび、getsuyoubi）
+    Tuesday＝火曜日（かようび、kayoubi）
+    Wednesday＝水曜日（すいようび、suiyoubi）
+    Thursday＝木曜日（もくようび、mokuyoubi）
+    Friday＝金曜日（きんようび、kinyoubi）
+    Saturday＝土曜日（どようび、doyoubi）
+"""
