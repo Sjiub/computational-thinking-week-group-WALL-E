d280f451684e9864235bc9615a1cd7c2f9d754f7
emscales <165669424+emscales@users.noreply.github.com>
2024-09-03 10:07:27 +0200

    Update station3.py

--- challenge_day2/station3.py
+++ challenge_day2/station3.py
@@ -1,2 +1,10 @@
 # number and boolean
 
+sample input          input           sample output              output
+10                    93151           F                          ?
+87                    80904           T                          ?
+85                    66255           F                          ?
+22                    69506           F                          ?
+81                    94030           T                          ?
+46                    48262           F                          ?
+30                    59010           T                          ?
