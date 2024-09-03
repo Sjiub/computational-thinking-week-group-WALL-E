52d2a02b4a5048db9f34706aa618586ea3c1bae8
shriya2k5 <140488778+shriya2k5@users.noreply.github.com>
2024-09-03 15:02:28 +0200

    Updated station6.py to only define the function

--- challenge_day2/station6.py
+++ challenge_day2/station6.py
@@ -6,7 +6,3 @@ def solution_station_6(input_list):
     output_values = [round(math.sin(x), 4) for x in input_list]
     return output_values
 
-inputs = [47, 28, 47, 58, 46, 15, 73, 72, 30, 22, 58, 38, 34, 35, 16, 46, 47, 28, 47, 91, 62, 72, 87, 78, 95, 49, 40, 24, 21]
-
-outputs = solution_station_6(inputs)
-
