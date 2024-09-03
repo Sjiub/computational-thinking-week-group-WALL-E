cc89959f3586bd40d04387bfa39609ab92ec04cc
shriya2k5 <140488778+shriya2k5@users.noreply.github.com>
2024-09-03 14:50:43 +0200

    station6 updated

---
+++ challenge_day2/station6.py
@@ -0,0 +1,12 @@
+#The sample outputs are the sines of the sample inputs. Therefore, the function calculates the sine of the inputs to give the desired output
+
+import math
+
+def solution_station_7(input_list):
+    output_values = [round(math.sin(x), 4) for x in input_list]
+    return output_values
+
+inputs = [47, 28, 47, 58, 46, 15, 73, 72, 30, 22, 58, 38, 34, 35, 16, 46, 47, 28, 47, 91, 62, 72, 87, 78, 95, 49, 40, 24, 21]
+
+outputs = solution_station_7(inputs)
+
