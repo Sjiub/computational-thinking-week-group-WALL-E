36608ef0258a18b4ea707d0b6d00cf1bcaba0679
Khushi <144587562+Khushiii891@users.noreply.github.com>
2024-09-03 20:21:50 +0700

    Update station4.py

--- challenge_day2/station4.py
+++ challenge_day2/station4.py
@@ -13,11 +13,11 @@ input          output
 #Check whether the number is a prime number or not
 def solution_station_4(number):
     if number <= 1: 
-        print('F') #return False for numbers that are 1 or 0 because they aren't prime numbers
+        return False
     else:
         for n in range(2, number):
             if number % n == 0: 
-                print('F') #if a divisor is found, the number is not prime 
+                return False #if a divisor is found, the number is not prime 
                 break
         else:
-            print('T') #else if a divisor is not found, it's prime 
+            return True #else if a divisor is not found, it's prime 
